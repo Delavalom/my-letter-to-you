d36a8facd5f4670725660bb405b8011f127388e1
Delavalom <laarvelo@icloud.com>
2023-05-30 18:33:02 -0400

    decouple components

--- src/pages/data/index.ts
+++ src/pages/data/index.ts
@@ -1,10 +1,71 @@
-import unVeranoSinTi from 'public/un_verano_sin_ti'
+import unVeranoSinTi from 'public/un_verano_sin_ti.webp'
+import perdoname from 'public/perdoname.jpeg'
+import malaMia from 'public/mala_mia.jpeg'
+import todoDeTi from 'public/todo_de_ti.jpeg'
+import xUltimaVez from 'public/x_ultima_vez.jpeg'
+import amoresComoElNuestro from 'public/amores_como_el_nuestro.jpeg'
+import dinero from 'public/dinero.jpeg'
+import chivirika from 'public/chivirika.jpeg'
+import type { StaticImageData } from 'next/image'
 
-type CardProperties = {
+export type CardProperties = {
      content: string
-     image
+     image?: StaticImageData
 }
 
 export default [
+    {
+        content: "Thais, no quiero pasar",
+    },
+    {
+        content: "un verano sin ti,",
+        image: unVeranoSinTi,
+    },
+    {
+        content: "perdoname,",
+        image: perdoname
+    },
+    {
+        content: "mala mia",
+        image: malaMia
+    },
+    {
+        content: "pero quiero"
+    },
+    {
+        content: "todo de ti,",
+        image: todoDeTi
+    },
+    {
+        content: "X ultima vez,",
+        image: xUltimaVez
+    },
+    {
+        content: "dame una oportunidad,",
+    },
+    {
+        content: "amores como el nuestro",
+        image: amoresComoElNuestro
+    },
+    {
+        content: "lo merecen"
+    },
+    {
+        content: "dejame demostrarte que ni el"
+    },
+    {
+        content: "dinero",
+        image: dinero
+    },
+    {
+        content: "ni una"
+    },
+    {
+        content: "chivirika,",
+        image: chivirika
+    },
+    {
+        content: "valen mas que tu para mi."
+    }
 
-]
+] satisfies CardProperties[]
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,125 +1,21 @@
 import Image from "next/image";
+import data, { type CardProperties } from "./data";
+import { FC } from "react";
 
 export default function Home() {
   return (
-    <div className="relative flex min-h-screen flex-col items-center justify-center overflow-hidden bg-gray-50 py-6 sm:py-12">
-      <div className="absolute inset-auto h-96 w-96 scale-150 bg-orange-200 opacity-20 blur-3xl"></div>
-
-      <div className="absolute inset-auto h-96 w-96 translate-x-full scale-150 bg-green-200 opacity-20 blur-3xl"></div>
-      <div className="w-full">
-        <div className="scrollbar-hide mt-14 flex w-full snap-x snap-mandatory scroll-px-10 gap-10 overflow-x-scroll scroll-smooth px-10">
-          <div className="md:2/3 relative aspect-[2/3] w-[90%] shrink-0 snap-start snap-always rounded-xl bg-orange-100 sm:w-[44%] md:w-[30%]">
-            <div className="absolute bottom-0 z-10 w-full rounded-xl bg-gradient-to-t from-black px-5 py-3">
-              <h2 className="mt-4 text-xl font-bold text-white">Cangu</h2>
-              <p className="text-sm text-white/50">Indonesia</p>
-            </div>
-            <Image
-              fill
-              src="/public/un_verano_sin_ti.webp"
-              alt="image"
-              className="h-full w-full rounded-xl object-cover"
-            />
-          </div>
-
-          <div className="md:2/3 relative aspect-[2/3] w-[90%] shrink-0 snap-start snap-always rounded-xl bg-orange-100 sm:w-[44%] md:w-[30%]">
-            <div className="absolute bottom-0 z-10 w-full rounded-xl bg-gradient-to-t from-black px-5 py-3">
-              <h2 className="mt-4 text-xl font-bold text-white">New York</h2>
-              <p className="text-sm text-white/50">United States</p>
-            </div>
-            <Image
-              fill
-              src="/public/perdoname.jpeg"
-              alt="image"
-              className="h-full w-full rounded-xl object-cover"
-            />
-          </div>
-
-          <div className="md:2/3 relative aspect-[2/3] w-[90%] shrink-0 snap-start snap-always rounded-xl bg-blue-100 sm:w-[44%] md:w-[30%]">
-            <div className="absolute bottom-0 z-10 w-full rounded-xl bg-gradient-to-t from-black px-5 py-3">
-              <h2 className="mt-4 text-xl font-bold text-white">London</h2>
-              <p className="text-sm text-white/50">United Kingdom</p>
-            </div>
-            <Image
-              fill
-              src="/public/mala_mia.jpeg"
-              alt="image"
-              className="h-full w-full rounded-xl object-cover"
-            />
-          </div>
-
-          <div className="md:2/3 relative aspect-[2/3] w-[90%] shrink-0 snap-start snap-always rounded-xl bg-green-100 sm:w-[44%] md:w-[30%]">
-            <div className="absolute bottom-0 z-10 w-full rounded-xl bg-gradient-to-t from-black px-5 py-3">
-              <h2 className="mt-4 text-xl font-bold text-white">Bali</h2>
-              <p className="text-sm text-white/50">Indonesia</p>
-            </div>
-            <Image
-              fill
-              src="/public/todo_de_ti.jpeg"
-              alt="image"
-              className="h-full w-full rounded-xl object-cover"
-            />
-          </div>
-
-          <div className="md:2/3 relative aspect-[2/3] w-[90%] shrink-0 snap-start snap-always rounded-xl bg-rose-100 sm:w-[44%] md:w-[30%]">
-            <div className="absolute bottom-0 z-10 w-full rounded-xl bg-gradient-to-t from-black px-5 py-3">
-              <h2 className="mt-4 text-xl font-bold text-white">
-                Times Square
-              </h2>
-              <p className="text-sm text-white/50">Singapore</p>
-            </div>
-            <Image
-              fill
-              src="/public/x_ultima_vez.jpeg"
-              alt="image"
-              className="h-full w-full rounded-xl object-cover"
-            />
-          </div>
-
-          <div className="md:2/3 relative aspect-[2/3] w-[90%] shrink-0 snap-start snap-always rounded-xl bg-yellow-100 sm:w-[44%] md:w-[30%]">
-            <div className="absolute bottom-0 z-10 w-full rounded-xl bg-gradient-to-t from-black px-5 py-3">
-              <h2 className="mt-4 text-xl font-bold text-white">Bangkok</h2>
-              <p className="text-sm text-white/50">Thailand</p>
-            </div>
-            <Image
-              fill
-              src="/public/amores_como_el_nuestro.jpeg"
-              alt="image"
-              className="h-full w-full rounded-xl object-cover"
-            />
-          </div>
-
-          <div className="md:2/3 relative aspect-[2/3] w-[90%] shrink-0 snap-start snap-always rounded-xl bg-red-100 sm:w-[44%] md:w-[30%]">
-            <div className="absolute bottom-0 z-10 w-full rounded-xl bg-gradient-to-t from-black px-5 py-3">
-              <h2 className="mt-4 text-xl font-bold text-white">Latina</h2>
-              <p className="text-sm text-white/50">South America</p>
-            </div>
-            <Image
-              fill
-              src="/public/dinero.jpeg"
-              alt="image"
-              className="h-full w-full rounded-xl object-cover"
-            />
-          </div>
-          <Card />
-        </div>
-      </div>
+    <div className="flex items-center justify-center h-screen w-screen bg-black overflow-x-auto scroll-smooth snap-mandatory snap-x relative gap-6">
+      {data.map((c) => (
+        <Card key={c.content} content={c.content} image={c.image} />
+      ))}
     </div>
   );
 }
 
-export const Card = () => {
+export const Card: FC<CardProperties> = ({ content, image }) => {
   return (
-    <div className="md:2/3 relative aspect-[2/3] w-[90%] shrink-0 snap-start snap-always rounded-xl bg-red-100 sm:w-[44%] md:w-[30%]">
-      <div className="absolute bottom-0 z-10 w-full rounded-xl bg-gradient-to-t from-black px-5 py-3">
-        <h2 className="mt-4 text-xl font-bold text-white">Latina</h2>
-        <p className="text-sm text-white/50">South America</p>
-      </div>
-      <Image
-        fill
-        src="/public/chivirika.jpeg"
-        alt="image"
-        className="h-full w-full rounded-xl object-cover"
-      />
+    <div className="snap-center shrink-0 first:pl-8 last:pr-8 w-full h-full bg-black p-6">
+      
     </div>
   );
 };
