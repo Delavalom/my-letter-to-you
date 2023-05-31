40014c99d7475ae48573de18afcadb37025b97c7
Delavalom <laarvelo@icloud.com>
2023-05-30 21:26:04 -0400

    add carousel

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,67 @@
 import Image from "next/image";
-import data, { type CardProperties } from "./data";
-import { FC } from "react";
+import unVeranoSinTi from "public/un_verano_sin_ti.webp";
+import { ChevronFirst, ChevronLast, Heart } from "lucide-react";
+import { Pause } from "lucide-react";
+import { useCarousel } from "use-carousel-hook";
+import { type RefObject } from "react";
 
 export default function Home() {
+  const { ref, previous, next, reset } = useCarousel();
+
   return (
-    <div className="flex items-center justify-center h-screen w-screen bg-black overflow-x-auto scroll-smooth snap-mandatory snap-x relative gap-6">
-      {data.map((c) => (
-        <Card key={c.content} content={c.content} image={c.image} />
-      ))}
-    </div>
+    <main className="w-full h-full flex flex-col gap-6 py-6 px-2 bg-black text-gray-100">
+      <div className="flex justify-center items-center gap-2">
+        <h2 className="font-medium">Una nota para Thais</h2>
+        <Heart className="stroke-green-500" />
+      </div>
+
+      <ul
+        ref={ref as RefObject<HTMLUListElement>}
+        className="w-full flex overflow-x-auto scrollbar-hide scroll-smooth snap-x snap-mandatory scroll-px-10 gap-2"
+      >
+        <div className="w-full relative shrink-0 snap-start snap-always bg-orange-100">
+          <div className="w-full h-full absolute bottom-0 z-10 flex items-center justify-center flex-col bg-gradient-to-t from-black">
+            <h2 className="mt-4 text-xl font-bold text-white">New York</h2>
+            <p className="text-sm text-white/50">United States</p>
+          </div>
+          <Image
+            src={unVeranoSinTi}
+            alt="un verano sin ti"
+            className="w-full h-full"
+          />
+        </div>
+
+        <Image
+          src={unVeranoSinTi}
+          alt="un verano sin ti"
+          className="w-full h-full"
+        />
+
+        <Image
+          src={unVeranoSinTi}
+          alt="un verano sin ti"
+          className="w-full h-full"
+        />
+
+        <Image
+          src={unVeranoSinTi}
+          alt="un verano sin ti"
+          className="w-full h-full"
+        />
+      </ul>
+
+      <div className="flex w-full justify-center items-center px-4">
+        <h1 className="mt-4 text-xl font-bold text-white">Un verano sin ti</h1>
+      </div>
+      <div className="flex w-full justify-evenly px-4">
+        <button onClick={() => previous()}>
+          <ChevronFirst className="border border-white rounded-full p-2 w-12 h-12" />
+        </button>
+        <Pause className="border border-white rounded-full p-2 w-12 h-12" />
+        <button onClick={() => next()}>
+          <ChevronLast className="border border-white rounded-full p-2 w-12 h-12" />
+        </button>
+      </div>
+    </main>
   );
 }
-
-export const Card: FC<CardProperties> = ({ content, image }) => {
-  return (
-    <div className="snap-center shrink-0 first:pl-8 last:pr-8 w-full h-full bg-black p-6">
-      
-    </div>
-  );
-};
